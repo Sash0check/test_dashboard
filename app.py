7ace46b25bcd133a2c3087c160e2e15a569cc947
Александр Абдулов <sanek_015@mail.ru>
2021-04-26 16:39:00 +0300

    updated project files

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
